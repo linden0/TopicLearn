a8fd6bf9f8184d75beb00a6cb0e512a9dafaa640
linden <linden.wang04@gmail.com>
2023-01-04 20:10:28 -0800

    refactoring

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
 
     async function logout() {
+        setIsNavExpanded(!isNavExpanded);
         localStorage.removeItem("token");
         setIsAuthenticated(false);
         await navigate("/login");
@@ -26,67 +27,57 @@ function Navbar() {
     return (
         <nav className="navigation">
             <Link to="/" className="brand-name">
-                Topic Learn
+                <h1>📘</h1>
+                <h1 className="blue-font no-wrap">Topic Learn</h1>
             </Link>
             <button className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="white"
-                >
-                <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
                 </svg>
             </button>
             <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
-                {
-                    isAuthenticated ? (
-                        <ul>
-                            <li>
-                                <Link to="/feed" className="links">Feed</Link>
-                            </li>
-                            <li>
-                                <Link to="/dashboard" className="links">Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link to="/liked" className="links">Liked</Link>
-                            </li>
-                        </ul>
-                    ) : (<ul></ul>)
-                }
+                <ul>
+                    {isAuthenticated ? (
+                    <>
+                    <li>
+                        <Link to="/feed" className="links" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Feed</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard" className="links" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/liked" className="links" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Liked</Link>
+                    </li>
+                    </>
+                    ) : (null)}
+                </ul>
                 
                 <ul>
-                {
-                    isAuthenticated ? 
-                    (
-                        <li>
-                            <button onClick={logout} className="button-filled">Sign Out</button>
-                        </li>
-                    ) : (
-                        <>
-                            <li>
-                                <button className="button-filled white-bg">
-                                    <Link to="/login" className="links">Sign In</Link>
-                                </button>
-                            </li>
-                            <li>
-                                <button className="button-filled">
-                                    <Link to="register" className="links white-font">Sign Up</Link>
-                                </button>
-                            </li>
-                        </>
-                    )
-                }
+                    {isAuthenticated ? (
+                    <li>
+                        <button onClick={logout} className="button-filled">Sign Out</button>
+                    </li>) : (
+                    <>
+                    <li>
+                        <button className="button-filled white-bg" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+                            <Link to="/login" className="links">Sign In</Link>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="button-filled" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+                            <Link to="register" className="links white-font">Sign Up</Link>
+                        </button>
+                    </li>
+                    </>
+                    )}
                 </ul>
             </div>
         </nav>
-
     )
 }
-
 
 export default Navbar;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -99,7 +99,6 @@ Router.post("/login", (req, res) => {
 
 function verifyJWT(req, res, next) {
   const token = req.headers["x-access-token"]?.split(' ')[1]
-  console.log(req.body)
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) return res.json({
